09c25216cdb8d2c523dd3047ba1a9e82219c7ec8
InensusDev <developer@inensus.com>
2022-11-15 15:47:45 +0300

    initial commit

---
+++ src/Models/WaveMoneyCredential.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Inensus\WaveMoneyPaymentProvider\Models;
+use App\Models\BaseModel;
+
+class WaveMoneyCredential extends BaseModel
+{
+    protected $table = 'wave_money_credentials';
+}
